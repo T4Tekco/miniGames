3186f9fec7353e351ed9044005cbfe507ce37630
kieuanhne <kieuanh06042002@gmail.com>
2023-04-04 09:12:01 +0700

    Commit nav_0404

--- app/src/main/java/com/android/t4tek/app/braingame/BrainGameActivity.kt
+++ app/src/main/java/com/android/t4tek/app/braingame/BrainGameActivity.kt
@@ -2,6 +2,7 @@ package com.android.t4tek.app.braingame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.t4tek.R
 
 class BrainGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
