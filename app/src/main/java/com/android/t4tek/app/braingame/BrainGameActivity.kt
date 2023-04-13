102ee27df0f1848bdd3ac6acae12cce435ac3d95
nvbao <0306201314@caothang.edu.vn>
2023-04-13 15:24:43 +0700

    fix UI

--- app/src/main/java/com/android/t4tek/app/braingame/BrainGameActivity.kt
+++ app/src/main/java/com/android/t4tek/app/braingame/BrainGameActivity.kt
@@ -3,10 +3,16 @@ package com.android.t4tek.app.braingame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.t4tek.R
+import com.android.t4tek.app.braingame.login.ChangePassFragment
+import com.android.t4tek.app.braingame.login.LoginFragment
+import com.android.t4tek.app.braingame.signup.SignUpFragment
+import com.android.t4tek.app.braingame.start.StartFragment
 
 class BrainGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brain_game)
+        val frmHome = StartFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frm,frmHome).commit()
     }
 }
