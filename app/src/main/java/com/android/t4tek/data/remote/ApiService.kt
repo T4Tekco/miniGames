5c62ffdc60a59521d4e214f451b948836154e26d
ptdung <phamtiendung652@gmail.com>
2023-04-25 10:22:44 +0700

    del file

--- app/src/main/java/com/android/t4tek/data/remote/ApiService.kt
+++ app/src/main/java/com/android/t4tek/data/remote/ApiService.kt
@@ -1,14 +1,10 @@
 package com.android.t4tek.data.remote
 
 import com.android.t4tek.data.remote.response.PeopleResponse
-import com.android.t4tek.data.remote.response.QuestionResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("astros.json")
     suspend fun getUsers(): Response<PeopleResponse>
-
-    @GET("question.json")
-    suspend fun getQuestion(): Response<QuestionResponse>
 }
